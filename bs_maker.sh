d9f36e1d8ab88bde751923a749075b240e60a5c7
holoitsme <bisio.97@gmail.com>
2020-05-25 13:18:48 +0200

    Naming of theme directory now follows official themes convention

--- bs_maker.sh
+++ bs_maker.sh
@@ -33,16 +33,18 @@ then
 	echo "spinner not specified, using default one"
 fi
 
-mkdir $NAME
-sed "s/template/$NAME/g" ".template/bootsplash-manjaro-template.sh" > "$NAME/bootsplash-manjaro-$NAME.sh"
-sed -i "s/logo.png/$(basename $LOGO)/g" "$NAME/bootsplash-manjaro-$NAME.sh"
-sed -i "s/spinner.gif/$(basename $SPINNER)/g" "$NAME/bootsplash-manjaro-$NAME.sh"
-sed "s/template/$NAME/g" ".template/bootsplash-manjaro-template.initcpio_install" > "$NAME/bootsplash-manjaro-$NAME.initcpio_install"
-sed "s/template/$NAME/g" ".template/PKGBUILD" > "$NAME/PKGBUILD"
-sed -i "s/logo.png/$(basename $LOGO)/g" "$NAME/PKGBUILD"
-sed -i "s/spinner.gif/$(basename $SPINNER)/g" "$NAME/PKGBUILD"
-sed "s/template/$NAME/g" ".template/README.md" > "$NAME/README.md"
-cp ".template/bootsplash-packer" "$NAME/bootsplash-packer"
-cp ".template/bootsplash-packer.rst" "$NAME/bootsplash-packer.rst"
-cp "$LOGO" "$NAME/$(basename $LOGO)"
-cp "$SPINNER" "$NAME/$(basename $SPINNER)"
+THDIR="manjaro-bootsplash-$NAME"
+mkdir $THDIR
+
+sed "s/template/$NAME/g" ".template/bootsplash-manjaro-template.sh" > "$THDIR/bootsplash-manjaro-$NAME.sh"
+sed -i "s/logo.png/$(basename $LOGO)/g" "$THDIR/bootsplash-manjaro-$NAME.sh"
+sed -i "s/spinner.gif/$(basename $SPINNER)/g" "$THDIR/bootsplash-manjaro-$NAME.sh"
+sed "s/template/$NAME/g" ".template/bootsplash-manjaro-template.initcpio_install" > "$THDIR/bootsplash-manjaro-$NAME.initcpio_install"
+sed "s/template/$NAME/g" ".template/PKGBUILD" > "$THDIR/PKGBUILD"
+sed -i "s/logo.png/$(basename $LOGO)/g" "$THDIR/PKGBUILD"
+sed -i "s/spinner.gif/$(basename $SPINNER)/g" "$THDIR/PKGBUILD"
+sed "s/template/$NAME/g" ".template/README.md" > "$THDIR/README.md"
+cp ".template/bootsplash-packer" "$THDIR/bootsplash-packer"
+cp ".template/bootsplash-packer.rst" "$THDIR/bootsplash-packer.rst"
+cp "$LOGO" "$THDIR/$(basename $LOGO)"
+cp "$SPINNER" "$THDIR/$(basename $SPINNER)"
